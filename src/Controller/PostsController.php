db8f6544d02f17590978b848f70f385b4f29bf89
Senpai-tn <60314408+Senpai-tn@users.noreply.github.com>
2020-10-15 14:43:06 +0100

    Heroku

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,13 +18,23 @@ class PostsController extends AbstractController
     }
 
     /**
-     * @Route("/", name="posts")
+     * @Route("/", name="posts",methods="GET|POST")
      */
-    public function index()
+    public function index(Request $r)
     {
-
+        $form = $this->createFormBuilder()
+            ->add("title")
+            ->add('submit',SubmitType::class)
+            ->getForm()
+        ;
+        $form->handleRequest($r);
+        if(($form->isSubmitted())&&($form->isValid()))
+        {
+            return $this->json(["ff"=>"sdfdsf"]);
+        }
         return $this->render('posts/index.html.twig', [
             'controller_name' => 'PostsController',
+            'form'=>$form->createView()
         ]);
     }
 }
